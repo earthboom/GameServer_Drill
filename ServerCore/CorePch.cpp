fa322e16c3fcd15750eebe8d13a04d3105b3ce4f
earthboom <milkywaycrush@gmail.com>
2021-10-13 00:11:43 +0900

    Setting

---
+++ ServerCore/CorePch.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "CorePch.h"
+
+void HelloWorld()
+{
+	cout << "게임 서버 시작이다!" << endl;
+}
---
+++ ServerCore/CorePch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// 다른 Project들에서 참조하길 원하는 header들을 모아 관리하기 위한 pch (PreCompiled header)
+
+#include "Types.h"
+
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+using namespace std;
+
+void HelloWorld();
